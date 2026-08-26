fab8b0f456fd3b9aa105e242bcf126f3b416dc0b
Fernanda Kelly <42969258+FernandaKelly@users.noreply.github.com>
2026-08-26 14:59:42 -0300

    Atualização.

--- Ativacao_Estudos_SQL.R
+++ Ativacao_Estudos_SQL.R
@@ -3,12 +3,16 @@ renv::install(DBI)
 library(duckdb)
 library(DBI)
 library(here)
+library(RSQLite)
+library(datasets)
 
 ###################
 setwd("C:/Users/fernanda-romeiro/OneDrive - Governo do Estado do Rio Grande do Sul/Projetos/SQL/SQL_project")
 here::set_here("C:/Users/fernanda-romeiro/OneDrive - Governo do Estado do Rio Grande do Sul/Projetos/SQL/SQL_project")
 ##################
 load("BR_SPEI_3.RData")
+
+mtcars <- datasets::mtcars
 ##################
 
 drv <- duckdb(dbdir = "quack.db")
@@ -29,3 +33,8 @@ DBI::dbWriteTable(
   as.data.frame(BR_SPEI_3),
   overwrite = TRUE
 )
+
+
+DBI::dbWriteTable(con,
+                  "mtcars",
+                  as.data.frame(mtcars))
